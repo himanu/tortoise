3eb3ccd871635dd4af2bf0db2efe7d101ef49abe
himanu <hy287719@gmail.com>
2022-07-07 11:24:35 +0530

    Styled the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="big">Type the Alphabet</div>
+        <div>
+          Typing Game to see how fast you type. Timer starts when you do :)
+        </div>
+      </div>
+      <div className="whitepad"></div>
+      <div className="timerScreen">
+        <div className="timer">
+          Time: 0.000s
+        </div>
+        <div>
+          My Best Time: 5s!
+        </div>
+      </div>
+      <div className="keyboard">
+        <input className="typeBox" type="text" placeholder="Type Here"></input>
+        <button className="resetBtn" type="button"> Reset </button>
+      </div>
     </div>
   );
 }
